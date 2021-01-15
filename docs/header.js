68d7ea648ea7aa829154108a1e8f442b41b8c3ad
片岡裕雄 <Hirokatsu@kataokahiroo-no-MacBook-Pro-2.local>
2021-01-15 13:41:32 +0900

    add major change

---
+++ docs/header.js
@@ -0,0 +1,5 @@
+document.write('\
+<div>\
+<a href="http://hirokatsukataoka.net/project/cc/index_cvpaperchallenge.html" target="_self"><img src="./img/cc_logo_2.png" style="margin-top: 5px; width:100%; height:auto; max-height:100px; max-width:525px;" /></a>\
+</div>\
+');
